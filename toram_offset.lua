b69f9e872a5d293304a45c9c723925354c4226c3
cabinz3q <namikyst@gmail.com>
2025-05-20 20:27:40 +0700

    Add files via upload

---
+++ toram_offset.lua
@@ -0,0 +1,134 @@
+-- class MobaPlayerSecondaryStatus :  | get_CriticalDmg
+o_p_cdm = 0x109B9E4
+-- class MobaPlayerSecondaryStatus :  | get_CriticalMagicDmg
+o_m_cdm = 0x109BD04
+-- class PlayerAttackBase :  | checkAbnormalPercent
+o_ailment_res = 0x10FD1D0
+-- class MobBattleStatus :  | get_ExpDefNormal
+o_mob_prorate_normal = 0xF986A4
+-- class MobBattleStatus :  | get_ExpDefSkill
+o_mob_prorate_skill = 0xF986BC
+-- class MobBattleStatus :  | get_ExpDefMagic
+o_mob_prorate_magic = 0xF986D4
+-- class BossMobBattleStatus :  | get_ExpDefNormal
+o_bos_prorate_normal = 0xF71730
+-- class BossMobBattleStatus :  | get_ExpDefSkill
+o_bos_prorate_skill = 0xF71748
+-- class BossMobBattleStatus :  | get_ExpDefMagic
+o_bos_prorate_magic = 0xF71760
+-- class PlayerAttackBase :  | CalcPowerResistDamage
+o_physp = 0x11086C0
+-- class PlayerAttackBase :  | CalcMagicResistDamage
+o_magip = 0x110878C
+-- class EnemyMobActionManagerBase :  | get_Size
+o_long_range_atk = 0xF7CB04
+-- class BattleManagerBase :  | CheckActionRange
+o_long_range_skill = 0x146B7A8
+-- class PlayerActionManager :  | get_MoveSpeed
+o_movement_speed = 0x10B1F7C
+-- class PlayerStatus :  | get_IsInvincibility
+o_godmode = 0x10F01A8
+-- class PlayerAttackBase :  | checkCriticalPercent
+o_p_crit_rate = 0x10FC804
+-- class PlayerAttackBase :  | CheckMagicCritical
+o_m_crit_rate = 0x110EAA4
+-- class PlayerSecondaryStatus :  | get_Aspd
+o_atknotime = 0x10E9190
+-- class PlayerSecondaryStatus :  | get_Cspd
+o_castnotime = 0x10E91CC
+-- class PlayerSecondaryStatus :  | GetMotionSpeed
+o_motion_speed = 0x10EC5B0
+-- class PlayerSecondaryStatus :  | get_Stable
+o_stability = 0x10E8570
+-- class PlayerSecondaryStatus :  | get_EqAtk
+o_eqattack = 0x10E8408
+-- class PlayerSecondaryStatus :  | get_Hit
+o_mobnoflee = 0x10E88DC
+-- class EnemyMobActionManagerBase :  | get_SystemInvinci
+o_mobnoinvicible = 0xF7CDF8
+-- class PlayerAttackBase :  | checkMobReaction
+o_mobnoevation = 0x10FB7FC
+-- class MobBattleSystemManager :  | CreateMobAction
+o_mobnomove = 0xDBB030
+-- class FieldScriptManager :  | ChangeEventScene
+o_autoskip1 = 0x157F1FC
+-- class FieldScriptManager :  | GetUIEventMessageWindow
+o_autoskip2 = 0x157F4D4
+-- class RoomManager :  | get_IsBattleEnd
+o_skipboss = 0x1238B34
+-- class MissionCommon :  | get_ItemList
+o_skipitem = 0x1FA7D08
+-- class QuestManager :  | GetScenarioIdList
+o_unlockmap = 0x11F9CFC
+-- class AuraBladeAction :  | CheckRangeHit
+o_aurablade_aoe = 0x126351C
+-- class AuraBladeAction :  | NextRangeHit
+o_aurablade_hit = 0x1263778
+-- class MeteoBreakerAction :  | CheckRangeHit
+o_meteor_aoe = 0x1269AC0
+-- class MeteoBreakerAction :  | NextRangeHit
+o_meteor_hit = 0x1269A54
+-- class CrossFireAction :  | CheckRangeHit
+o_crossfire_aoe = 0x13439E8
+-- class CrossFireAction :  | NextRangeHit
+o_crossfire_hit = 0x1343978
+-- class DecoyShooterAction :  | CheckRangeHit
+o_decoy_aoe = 0x13463B8
+-- class BlizzardAction :  | CheckRangeHit
+o_blizzard_aoe = 0x1359674
+-- class MagicImpactAction :  | NextRangeHit
+o_blizzard_hit = 0x12F89F8
+-- class MagicFinawAction :  | CheckRangeHit
+o_mimpact_aoe = 0x12EDBB8
+-- class MagicFinawAction :  | CheckRangeHit
+o_mfinale_aoe = 0x12EDBB8
+-- class MagicCannonAction :  | NextRangeHit
+o_mfinale_hit = 0x12E83B8
+-- class MagicCannonAction :  | CheckRangeHit
+o_mcannon_aoe = 0x12E8098
+-- class MagicFallAction :  | NextRangeHit
+o_mcannon_hit = 0x12EB7BC
+-- class MagicFallAction :  | CheckRangeHit
+o_mcrash_aoe = 0x12EB4BC
+-- class HassohappaAction :  | NextRangeHit
+o_mcrash_hit = 0x130FE1C
+-- class HassohappaAction :  | CheckRangeHit
+o_hassohappa_aoe = 0x130FC4C
+-- class UnionSwordAction :  | NextRangeHit
+o_hassohappa_hit = 0x12DEFD8
+-- class UnionSwordAction :  | CheckRangeHit
+o_union_aoe = 0x12DF054
+-- class SoulHuntAction :  | NextRangeHit
+o_union_hit = 0x1280CA8
+-- class SoulHuntAction :  | CheckRangeHit
+o_soulhunter_aoe = 0x1280FB4
+-- class LunaDitherStarAction :  | NextRangeHit
+o_soulhunter_hit = 0x128A5D0
+-- class LunaDitherStarAction :  | CheckRangeHit
+o_lunar1_aoe = 0x128A0E0
+-- class LunaDitherStarBladeRainAction :  | NextRangeHit
+o_lunar1_hit = 0x128B85C
+-- class LunaDitherStarBladeRainAction :  | CheckRangeHit
+o_lunar2_aoe = 0x128B5F8
+-- class TwinBusterBladeAction :  | NextRangeHit
+o_lunar2_hit = 0x12A250C
+-- class TwinBusterBladeAction :  | CheckRangeHit
+o_twinb_aoe = 0x12A23D4
+-- class DimensionTillAction :  | NextRangeHit
+o_twinb_hit = 0x12B282C
+-- class DimensionTillAction :  | CheckRangeHit
+o_dimension_aoe = 0x12B26BC
+-- class DiveImpactAction :  | NextRangeHit
+o_dimension_hit = 0x12B418C
+-- class DiveImpactAction :  | CheckRangeHit
+o_dive_aoe = 0x12B4060
+-- class DragonicChargeAction :  | NextRangeHit
+o_dive_hit = 0x12B67B4
+-- class DragonicChargeAction :  | CheckRangeHit
+o_dragonc_aoe = 0x12B652C
+-- class SatelliteArrowAction :  | NextRangeHit
+o_dragonc_hit = 0x12C9CD8
+-- class SatelliteArrowAction :  | CheckRangeHit
+o_satelite_aoe = 0x12C9BA4
+-- class SnipingAction :  | NextRangeHit
+o_satelite_hit = 0x13573C8
